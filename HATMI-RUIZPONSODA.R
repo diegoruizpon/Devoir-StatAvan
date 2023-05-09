9be30b75ccc2961f7fbd16256df80e55af8015bd
Diego Ruiz <diego.ruiz-ponsoda@student-cs.fr>
2023-05-09 10:58:28 +0200

    modified:   HATMI-RUIZPONSODA.R

--- HATMI-RUIZPONSODA.R
+++ HATMI-RUIZPONSODA.R
@@ -1,5 +1,5 @@
 # setwd ...
-# setwd("C:/Users/diego/OneDriveUPM/MII/CentraleSupelec/StatAvan")
+# setwd("C:/Users/diego/OneDriveUPM/MII/CentraleSupelec/StatAvan/dm")
 
 
 # install.packages("AppliedPredictiveModeling")
@@ -54,12 +54,80 @@ abline(h=0,lty=2)
 
 anova(Mod1, Mod2)
 
+## Exercise 2
+
+# Calculate training errors 
+
+sum(Mod1$residuals^2)/length(Mod1$residuals)
+
+sum(Mod2$residuals^2)/length(Mod2$residuals)
+
+## Explain code
 
 
+# Leave-one-out cross-validation (Prediction Residual Sum of squares)
+n = nrow(cars2010)
+loo1=0
+for(j in 1:n){
+  #  LM omitting the j-th row 
+  r1 = lm(FE ~ EngDispl, data=cars2010[-j,])
+  
+  
+  loo1=loo1+sum((cars2010$FE[j]-predict(r1,cars2010[j,]) )^2 )
+}
 
+# Forecast package
+library(forecast)
 
+CV(Mod1)
 
+CV(Mod2)
 
+#Error calculation (Mod2 is still better)
+
+sum((cars2011$FE - predict(Mod1, cars2011))^2)/nrow(cars2011)
+
+sum((cars2011$FE - predict(Mod2, cars2011))^2)/nrow(cars2011)
+
+## Ex 3. Modele total
+
+Mtot=lm(FE~.,data=cars2010)
+
+summary(Mtot)
+
+par(mfrow=c(1,3)) 
+
+plot(Mtot$fitted,Mtot$residuals,main="differents residus")
+abline(h=0,lty=2)
+
+library(MASS)
+
+qqnorm(studres(Mtot),main="qqplot")
+qqline(stdres(Mtot))
+
+hist(Mtot$residuals)
+
+library(timeDate)
+hist(Mtot$residuals)
+
+# Kurtosis!!
+
+## Ex 4. 
+
+stepAIC(Mtot)
+
+BIC(Mtot)
+
+## Ex. 5
+
+library(leaps)
+
+recherche=regsubsets(FE~.,int=TRUE, nbest=1, nvmax=50,method="exhaustive",data=cars2010)
+
+par(mfrow=c(1,1)) 
+
+plot(recherche,scale="bic")
+plot(summary(recherche)$bic)
 
 
 
